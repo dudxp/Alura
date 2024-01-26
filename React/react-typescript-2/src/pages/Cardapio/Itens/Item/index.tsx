a923eb195df131f360ccc3872f0e18bb28117fe7
Eduardo <dud_boeira@hotmail.com>
2024-01-25 22:53:57 -0300

    12333

--- React/react-typescript-2/src/pages/Cardapio/Itens/Item/index.tsx
+++ React/react-typescript-2/src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,7 +1,7 @@
 import IItemCardapio from "util/itemCardapio";
 import style from "./Item.module.scss";
-import logo from "assets/logo.svg";
 import item from "../itens.json";
+import classNames from "classnames";
 
 type Props = typeof item[0];
 
@@ -16,25 +16,31 @@ export default function Item(props: Props) {
         id,
         category
         } = props;
-
   return (
     <div className={style.item}>
       <div className={style.item__imagem}>
-        <img src={logo} alt="Imagem do item" />
+        <img src={photo} alt={title} />
       </div>
       <div className={style.item__descricao}>
         <div className={style.item__titulo}>
           <h2>{title} </h2>
           <p>{description}</p>
         </div>
-
         <div className={style.item__tags}>
-          <div className={style.item__tipo}>{category.label}</div>
+          <div className={classNames(
+            style.item__tipo,
+            style[`item__tipo__${category.label.toLowerCase()}`]
+            // [style.item__tipo__massas]: category.label === "Massas",
+            // [style.item__tipo__carnes]: category.label === "Carnes",
+            // [style.item__tipo__combos]: category.label === "Combos",
+            // [style.item__tipo__veganos]: category.label === "Veganos"
+          )}
+          >{category.label}</div>
           <div className={style.item__porcao}>{size}g</div>
           <div className={style.item__qtdpessoas}>
-            Serve {serving} pessoas
+            Serve {serving} pessoa{serving > 1 ? "s" : ""} 
           </div>
-          <div className={style.item__valor}>R${price}</div>
+          <div className={style.item__valor}>R${price.toFixed(2)}</div>
         </div>
       </div>
     </div>
