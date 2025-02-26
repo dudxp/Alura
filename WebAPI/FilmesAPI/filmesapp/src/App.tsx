e37fdda2d3207eb33621df68f8d1eb636f77957e
Eduardo <dud_boeira@hotmail.com>
2025-02-26 17:59:07 -0300

    *Criada parte em React do programa

---
+++ WebAPI/FilmesAPI/filmesapp/src/App.tsx
@@ -0,0 +1,16 @@
+import { Grid, Paper } from "@mui/material";
+
+function App() {
+  return (
+    <Grid 
+      container
+      spacing={2}
+      textAlign={"center"}>
+      <Grid>
+        <Paper>Teste</Paper>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default App;
